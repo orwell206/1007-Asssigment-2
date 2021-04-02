62c0033dbf844dd95691bc93f15b2edba2aa869d
raymousray <61280887+raymousray@users.noreply.github.com>
2021-04-02 15:15:16 +0800

    Add files via upload

---
+++ q4deadlock.c
@@ -0,0 +1,161 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define PROCESSES 5
+#define RESOURCES 5
+
+//SEMAPHORE
+char SEMAPHORE_NAME[RESOURCES][20];
+char FILENAME[RESOURCES][20];
+
+void writing_process(int num)
+{
+    /* 
+    num -> prcoess number
+    function to write to 2 different files
+
+     */
+    sleep(2);
+    int process_num = num;            /* reference process number */
+    int filenum, filenum2;            /* to reference the file number to be open */
+    char filename[20], filename2[20]; /* to reference the named file to be open with *fp */
+    char semName[20];                 /* named semaphore */
+    int sem_value, sem_value2;        /*      use for referencing semaphore num          */
+    sem_t *sem1, *sem2;               /*      use for referencing named semaphore by combining   test_ + sem_value      */
+    FILE *fp;                         /* file pointer */
+    printf("process %d create \n", process_num);
+    for (int i = 0; i < 3; i++)
+    {
+
+        switch (process_num)
+        /* to assure there will always be a deadlock: 
+        process 0 and process 1 will cause a deadlock for accessing each other resources 
+        file 1 -> process 0 -> file 2 -> process 1 -> file 1
+        process 2, process 3 and process 4 will cause a deadlock for accessing each other resources 
+        file 0 -> process 2 -> file 3 -> process 4 -> file 4 -> process 3 -> file 0 
+        */
+        {
+        case 0:
+            filenum = 1;
+            filenum2 = 2;
+            break;
+
+        case 1:
+            filenum = 2;
+            filenum2 = 1;
+            break;
+
+        case 2:
+            filenum = 0;
+            filenum2 = 3;
+            break;
+
+        case 3:
+            filenum = 4;
+            filenum2 = 0;
+            break;
+
+        case 4:
+            filenum = 3;
+            filenum2 = 4;
+            break;
+
+        default:
+            exit;
+        }
+
+        sem1 = sem_open(SEMAPHORE_NAME[filenum], O_EXCL);
+        sem2 = sem_open(SEMAPHORE_NAME[filenum2], O_EXCL);
+
+        while (1)
+        /* the no hold and wait deadlock prevention method */
+        {
+            if (sem_trywait(sem1) == 0)
+            {
+                if (sem_trywait(sem2) == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    sem_post(sem1);
+                    sleep(2);
+                }
+            }
+            else
+            {
+                sleep(2);
+            }
+        }
+
+        printf("Process %d accessing %s \n", process_num, FILENAME[filenum]);
+        fp = fopen(FILENAME[filenum], "a");
+        fprintf(fp, "Process %d was here during loop %d\n", process_num, i);
+        fclose(fp);
+
+        printf("Process %d accessing %s \n", process_num, FILENAME[filenum2]);
+        fp = fopen(FILENAME[filenum2], "a");
+        fprintf(fp, "Process %d was here during loop %d\n", process_num, i);
+        fclose(fp);
+
+        sem_post(sem1);
+        sem_post(sem2);
+        printf("Process %d released %s and %s \n", process_num, FILENAME[filenum], FILENAME[filenum2]);
+
+        sleep(2);
+    }
+    printf("Process %d finished running. Exiting program.\n", process_num);
+}
+
+int main()
+{
+    pid_t pid[PROCESSES], cpid;
+    int i, process[PROCESSES], stats = 0;
+    FILE *fp;
+    for (i = 0; i < RESOURCES; i++)
+    {
+        /*
+        print the filename and named semapohre into their respective arrays.
+        this is to creat/open the file we are using for this program. 
+        fopen(filename, "w"); -> to create/erase the file content.
+        */
+        process[i] = i + 1;
+        snprintf(SEMAPHORE_NAME[i], 20, "test_%d", i);
+        sem_open(SEMAPHORE_NAME[i], O_CREAT, 0777, 1);
+        snprintf(FILENAME[i], 20, "test_%d.txt", i);
+        fp = fopen(FILENAME[i], "w");
+        fclose(fp);
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        /* 
+        multiprocessing with fork. 
+        pid[i] = fork() will create the child process
+        if (pid[i] == 0) will only allow child process to execute the function then exiting the process
+         */
+        pid[i] = fork();
+        if (pid[i] == 0)
+        {
+            writing_process(i);
+            exit(i);
+        }
+    }
+
+    for (i = 0; i < 5; i++)
+    /* 
+    Parent waiting for child program to finish before terminating
+     */
+    {
+        cpid = waitpid(pid[i], &stats, 0);
+        printf("Child %d terminated with Process: %d\n",
+               cpid, WEXITSTATUS(stats));
+    }
+    return 0;
+}
